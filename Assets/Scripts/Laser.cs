e65b9c0c462de261164bbad3bf66c3af1c135133
Kevin Veen <kevinveen12@gmail.com>
2020-04-27 13:35:15 +0200

    Added basic the dark side laser with laser effect. May the force be with you.

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     private LineRenderer lr;
+    private float textureOffset = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        //create line
         lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //update line pos
         lr.SetPosition(0, transform.position);
+
+        //if line hits, shorten line to hit object
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
@@ -28,5 +33,13 @@ public class Laser : MonoBehaviour
         {
             lr.SetPosition(1, transform.forward * 5000);
         }
+
+        //pan texture
+        textureOffset -= Time.deltaTime * 2f;
+        if(textureOffset < -10f)
+        {
+            textureOffset += 10f;
+        }
+        lr.sharedMaterials[1].SetTextureOffset("_MainTex", new Vector2(textureOffset, 0f));
     }
 }
