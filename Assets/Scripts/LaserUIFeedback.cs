c07f20f1ccfd7777b6a8a5c90dc79dff0cad013a
Laurin <lgo1833@students.fhv.at>
2020-05-17 22:35:06 +0200

    Script for Cooldown UIFeedback

---
+++ Assets/Scripts/LaserUIFeedback.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserUIFeedback : MonoBehaviour
+{
+    public Image imageCooldown;
+    public float laserCooldown;
+    private ShootingScript shootingScript;
+    
+    
+
+    void Awake()
+    {
+        shootingScript = GameObject.FindObjectOfType<ShootingScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shootingScript.activateCooldown)
+        {
+            imageCooldown.fillAmount += 1 / laserCooldown * Time.deltaTime;
+
+            if(imageCooldown.fillAmount >=1)
+            {
+                if (!shootingScript.shooting)
+                {
+                    
+                    shootingScript.activateCooldown = false;
+                }
+
+                imageCooldown.fillAmount = 0;
+                
+            }
+        }
+         
+    }
+
+   
+    }
+
